aa6da2883f11109c5df15eb375570f81c62780ec
lockedleer <Daniel.Tyler.Brennan@gmail.com>
2020-12-15 19:00:21 -0500

    Adding error reporting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { onError } from "./libs/errorLib";
 import { useHistory } from "react-router-dom";
 import { AppContext } from "./libs/contextLib";
 import { LinkContainer } from "react-router-bootstrap";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./App.css";
 
 function App() {
@@ -67,9 +68,11 @@ function App() {
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
-          <Routes />
-        </AppContext.Provider>
+        <ErrorBoundary>
+          <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+            <Routes />
+          </AppContext.Provider>
+        </ErrorBoundary>
       </div>
     )
   );
--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,43 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({
+    dsn: "https://b10b0ef3dce347f883c4236578d3a7f4@o492060.ingest.sentry.io/5558690"
+  });
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
 export function onError(error) {
-    let message = error.toString();
+  let errorInfo = {};
+  let message = error.toString();
 
-    // Auth errors
-    if (!(error instanceof Error) && error.message) {
-      message = error.message;
-    }
+  // Auth errors
+  if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
+    message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
+  }
 
-    alert(message);
+  logError(error, errorInfo);
+
+  alert(message);
 }
